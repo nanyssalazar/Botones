59f282ad99289777fa620268dcc81b5aa8c0e3c8
ISND 8vo ISND89 9no Semestre <ISND89@AISND20-03.campus.iest.edu.mx>
2023-02-01 16:08:11 -0600

    Added image with function

--- app/src/main/java/edu/iest/botones/activity_segunda.kt
+++ app/src/main/java/edu/iest/botones/activity_segunda.kt
@@ -4,27 +4,72 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
-class activity_segunda : AppCompatActivity() {
+class activity_segunda : AppCompatActivity(), View.OnClickListener {
+
+    private var cambioIcono: Boolean = false
+    private lateinit var ivLogo: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
 
         // para obtener informacion de otro lado usamos el intent con esta funcion dependiendo el tipo de dato
         var fraseRecibida = intent.getStringExtra("frase")
-        var edad = intent.getIntExtra("edad",0)
+        var edad = intent.getIntExtra("edad", 0)
 
 
-        Log.d("DATOS","Datos recibidos con exito fueron frase $fraseRecibida y $edad")
+        Log.d("DATOS", "Datos recibidos con exito fueron frase $fraseRecibida y $edad")
 
 
         var bnAbrirPantalla = findViewById<Button>(R.id.bnPantalla)
+
+
+        ivLogo = findViewById<ImageView>(R.id.ivLogo)
+        /*  ivLogo.setOnClickListener {
+            var arregloImagenes = arrayOf<Int>(R.drawable.anahuac, R.drawable.iest)
+            if (cambioIcono) {
+                ivLogo.setImageResource(arregloImagenes[0])
+            } else {
+                ivLogo.setImageResource(arregloImagenes[1])
+            }
+            cambioIcono = !cambioIcono
+        }
+
         bnAbrirPantalla.setOnClickListener {
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
-        }
+            finish()
+        }*/
+
         var bnCerrar = findViewById<Button>(R.id.bnCerrar)
-        bnCerrar.setOnClickListener { finish() }
+        // bnCerrar.setOnClickListener { finish() }
+
+        bnCerrar.setOnClickListener(
+            this
+        )
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.bnCerrar -> finish()
+            R.id.bnPantalla -> {
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+                finish()
+            }
+        }
+        ivLogo.setOnClickListener {
+            var arregloImagenes = arrayOf<Int>(R.drawable.anahuac, R.drawable.iest)
+            if (cambioIcono) {
+                ivLogo.setImageResource(arregloImagenes[0])
+            } else {
+                ivLogo.setImageResource(arregloImagenes[1])
+            }
+            cambioIcono = !cambioIcono
+        }
     }
 }
